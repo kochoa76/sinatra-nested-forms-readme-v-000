f3279ce76954cbe162acf1d7fa8d8b2110fc1607
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-17 00:00:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,19 +5,18 @@ class App < Sinatra::Base
   get "/" do
     erb :new
   end
-
+  
   post '/student' do
-
     @student = Student.new(params[:student])
-
+   
     params[:student][:courses].each do |details|
       Course.new(details)
     end
-
-    @course = Course.all
+   
+    @courses = Course.all
+   
     erb :student
   end
-
 
 
 end
